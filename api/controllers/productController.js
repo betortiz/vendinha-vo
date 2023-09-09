2e74fca3f692c611f996a2abc62c9110a82f4bf0
betortizPSG <betortiz_PSG@outlook.com.br>
2023-09-09 16:10:35 -0400

    CRUD Product

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -1,6 +1,7 @@
 import productModel from '../models/productModel.js';
 import slugify from 'slugify';
 
+// Criar produto
 export const createProductController = async (req, res) => {
   try {
     // Dados do produto
@@ -32,6 +33,95 @@ export const createProductController = async (req, res) => {
     res.status(500).send({
       succcess: false,
       message: 'Erro ao criar produto',
+      error: error.message,
+    });
+  }
+};
+
+// Listar produtos
+export const getProductController = async (req, res) => {
+  try {
+    // Listar produtos
+    const products = await productModel.find({}).limit(10).sort({ createdAt: -1 });
+    // Resposta
+    res.status(200).send({
+      success: true,
+      counTotal: products.length,
+      message: 'Produtos listados com sucesso',
+      products,
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      succcess: false,
+      message: 'Erro ao listar produtos',
+      error: error.message,
+    });
+  }
+}
+
+// Buscar um produto
+export const getSingleProductController = async (req, res) => {
+  try {
+    // Buscar produto
+    const product = await productModel.findOne({ slug: req.params.slug });
+    // Resposta
+    res.status(200).send({
+      success: true,
+      message: 'Produto encontrado com sucesso',
+      product,
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      succcess: false,
+      message: 'Erro ao buscar produto',
+      error: error.message,
+    });
+  }
+};
+
+// Deletar produto
+export const deleteProductController = async (req, res) => {
+  try {
+    // Deletar produto
+    const product = await productModel.findOneAndDelete({ slug: req.params.slug });
+    // Resposta
+    res.status(200).send({
+      success: true,
+      message: 'Produto deletado com sucesso',
+      product,
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      succcess: false,
+      message: 'Erro ao deletar produto',
+      error: error.message,
+    });
+  }
+};
+
+// Atualizar produto
+export const updateProductController = async (req, res) => {
+  try {
+    // Atualizar produto
+    const product = await productModel.findOneAndUpdate(
+      { slug: req.params.slug },
+      req.fields,
+      { new: true }
+    );
+    // Resposta
+    res.status(200).send({
+      success: true,
+      message: 'Produto atualizado com sucesso',
+      product,
+    });
+  } catch (error) {
+    console.log(error)
+    res.status(500).send({
+      succcess: false,
+      message: 'Erro ao atualizar produto',
       error: error.message,
     });
   }
--- api/routes/productRoutes.js
+++ api/routes/productRoutes.js
@@ -1,12 +1,40 @@
 import express from 'express';
 import { requireSignIn } from './../middlewares/authMiddleware.js';
-import { createProductController } from '../controllers/productController.js';
+import {
+  createProductController,
+  deleteProductController,
+  getProductController,
+  getSingleProductController,
+  updateProductController,
+} from '../controllers/productController.js';
 import formidable from 'express-formidable';
 
 const router = express.Router();
 
 // routes
-router.post('/create-product', requireSignIn, formidable(), createProductController);
+// cadastrar produto
+router.post(
+  '/create-product',
+  requireSignIn,
+  formidable(),
+  createProductController
+);
 
+// Listar produtos
+router.get('/get-product', requireSignIn, getProductController);
+
+// Buscar um produto
+router.get('/get-product/:slug', requireSignIn, getSingleProductController);
+
+// Deletar produto
+router.delete('/delete-product/:slug', requireSignIn, deleteProductController);
+
+// Atualizar produto
+router.put(
+  '/update-product/:slug',
+  requireSignIn,
+  formidable(),
+  updateProductController
+);
 
 export default router;
